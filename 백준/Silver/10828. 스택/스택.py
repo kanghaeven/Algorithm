2eba1a809559d0cffd7942adb37142d433602ed4
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-01-26 23:20:06 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+stack = []
+n = int(input())
+
+for i in range(n):
+    user = sys.stdin.readline().split()
+
+    if user[0] == 'push':
+        stack.append(user[1])
+    
+    elif user[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+
+    elif user[0] == 'size':
+        print(len(stack))
+
+    elif user[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif user[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
