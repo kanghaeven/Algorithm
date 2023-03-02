6c5557577ae3cd49888dbbe7b7baedf5143631ee
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-03-02 09:54:02 +0900

    [Silver II] Title: 종이의 개수, Time: 4716 ms, Memory: 69484 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,27 +1,25 @@
-m_cnt = 0
-z_cnt = 0
-p_cnt = 0
+m_cnt, z_cnt, p_cnt = 0, 0, 0
 
-def check(x, y, N):
-    first = paper[x][y]
+def check(x, y, N): # 종이가 모두 같은 수로 되어 있는지 확인
+    first = paper[x][y] # 첫 값이랑 비교
 
-    for i in range(x, x + N):
+    for i in range(x, x + N): # x부터 x+N까지 순회
         for j in range(y, y + N):
-            if paper[i][j] != first:
-                return False
-    return True
+            if paper[i][j] != first: # 같지 않으면
+                return False # 거짓 반환
+    return True # 다 같으면 참 반환
 
-def scissors(x, y, N):
+def scissors(x, y, N): # 같은 크기의 종이 9개로 자르고, 확인 과정 반복
     global m_cnt, z_cnt, p_cnt
-    if check(x, y, N) == True:
+    if check(x, y, N): # 모두 같은 수로 되어있으면 개수 카운트
         if paper[x][y] == -1:
             m_cnt += 1
         if paper[x][y] == 0:
             z_cnt += 1
         if paper[x][y] == 1:
             p_cnt += 1
-        return
-
+        return # 함수 끝내줌
+    # 아니면 같은 크기의 9개로 자르기
     new_N = N // 3
     scissors(x, y, new_N)
     scissors(x, y + new_N, new_N)
@@ -35,7 +33,6 @@ def scissors(x, y, N):
 
 N = int(input())
 paper = [list(map(int, input().split())) for _ in range(N)]
-
 scissors(0, 0, N)
 print(m_cnt)
 print(z_cnt)
