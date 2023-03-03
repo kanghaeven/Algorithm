cdc3f8634b9a7235bbb3f9c0a6242ad8a96a24d5
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-03-03 12:00:42 +0900

    [Gold V] Title: 토마토, Time: 2004 ms, Memory: 178936 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,48 +2,46 @@ dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
 from collections import deque
-def bfs(one): # list에 토마토가있어
-    global flag
-    level = 0
+def bfs(one): # 익은 토마토(들) (동시에) 탐색
+    level = 0 # 날짜
     queue = deque()
     for j in one:
-        queue.append([j[0], j[1], level]) # append 할 때 토마토를 넣어주면 되겠지..?
-        visited[j[0]][j[1]] = True
+        queue.append([j[0], j[1], level]) # 각각 익은 토마토 탐색 위해 추가
+        visited[j[0]][j[1]] = True # 방문 처리
 
-########################################## 레벨은 어디다가 넣어야 할까요...?
     while queue:
-        ripe = queue.popleft() 
-        level = ripe[2]
-        for i in range(4):
+        ripe = queue.popleft() # 탐색 대상
+        level = ripe[2] # 날짜 업데이트
+        for i in range(4): # 사방 탐색
             nx = ripe[0] + dx[i]
             ny = ripe[1] + dy[i]
             if 0 <= nx < N and 0 <= ny < M:
-                if box[nx][ny] == 0:
+                if box[nx][ny] == 0: # 안 익었으면 익혀
                     box[nx][ny] = 1
                     visited[nx][ny] = True
-                    queue.append([nx, ny, level + 1]) # ...? = Node == level = ?
-
-    return level
+                    # 그대로 cnt 넣으면 4번 카운트됨
+                    queue.append([nx, ny, level + 1]) # 다음 탐색 대상과 날짜 카운트
+    return level # 날짜 반환
 
 M, N = map(int, input().split()) # M 가로칸 수, N 세로칸 수 
-box = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False for _ in range(M)] for _ in range(N)]
+box = [list(map(int, input().split())) for _ in range(N)] # 토마토 상태
+visited = [[False for _ in range(M)] for _ in range(N)] # 방문 처리
 flag = True
 
-one = []
+one = [] # 익은 토마토 리스트
 for x in range(N):
     for y in range(M):
         if box[x][y] == 1:
             one.append([x, y])
             
-result = bfs(one)
+result = bfs(one) # 최소 날짜 구하기
 
 for xx in range(N):
     for yy in range(M):
-        if box[xx][yy] == 0:
+        if box[xx][yy] == 0: # 탐색한 후에도 모두 익지 못하는 상황
             flag = False 
 
 if flag == True:
     print(result)
-elif flag == False:
+else:
     print(-1)
