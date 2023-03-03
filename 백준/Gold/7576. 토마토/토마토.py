78c456873ceefadc795545f11b8f4d0a089aef2f
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-03-03 19:13:36 +0900

    [Gold V] Title: 토마토, Time: 1860 ms, Memory: 170304 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,3 +1,4 @@
+
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
@@ -7,7 +8,6 @@ def bfs(one): # 익은 토마토(들) (동시에) 탐색
     queue = deque()
     for j in one:
         queue.append([j[0], j[1], level]) # 각각 익은 토마토 탐색 위해 추가
-        visited[j[0]][j[1]] = True # 방문 처리
 
     while queue:
         ripe = queue.popleft() # 탐색 대상
@@ -18,14 +18,12 @@ def bfs(one): # 익은 토마토(들) (동시에) 탐색
             if 0 <= nx < N and 0 <= ny < M:
                 if box[nx][ny] == 0: # 안 익었으면 익혀
                     box[nx][ny] = 1
-                    visited[nx][ny] = True
                     # 그대로 cnt 넣으면 4번 카운트됨
                     queue.append([nx, ny, level + 1]) # 다음 탐색 대상과 날짜 카운트
     return level # 날짜 반환
 
 M, N = map(int, input().split()) # M 가로칸 수, N 세로칸 수 
 box = [list(map(int, input().split())) for _ in range(N)] # 토마토 상태
-visited = [[False for _ in range(M)] for _ in range(N)] # 방문 처리
 flag = True
 
 one = [] # 익은 토마토 리스트
@@ -45,3 +43,4 @@ if flag == True:
     print(result)
 else:
     print(-1)
+
