f0b92a8de0c3bc2cd3644d684749383dd55c4bab
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-04-14 23:47:34 +0900

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString();
+
+
+
+function factorial(num) {
+    if (num <= 1) {
+        return 1;
+    }
+
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(input));
