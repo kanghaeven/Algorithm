000cd4828e6e7538b09cc53a70913316bb91a28f
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-04-17 16:53:22 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 972 ms, Memory: 157640 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -2,14 +2,18 @@ from bisect import bisect_left
 N = int(input())
 arr = list(map(int, input().split()))
 
-dp = [1]
-x = [arr[0]]
+x = [arr[0]] # 증가하는 부분 수열
 
 for i in range(1, N):
-    if arr[i] > x[-1]: # 현재 값이 x 배열의 마지막 값보다 클 경우
+    # 현재 값이 x 배열의 마지막 값보다 클 경우
+    if arr[i] > x[-1]: 
         x.append(arr[i])
-        dp.append(dp[-1] + 1)
-    else: # 현재 값이 x 배열의 마지막 값보다 작을 경우
-        idx = bisect_left(x, arr[i]) # 현재 값이 x 배열의 몇 번째 인덱스에 들어갈 수 있는지 찾아서
-        x[idx] = arr[i] # x 배열의 ids 위치에 현재 값을 넣어줌
+
+    # 현재 값이 x 배열의 마지막 값보다 작을 경우
+    else:
+        # 현재 값 arr[i]이 오름차순으로 x 배열의 몇 번째 인덱스에 들어갈 수 있는지 = idx
+        idx = bisect_left(x, arr[i])
+        x[idx] = arr[i] # x 배열의 idx 위치에 현재 값을 넣어줌
+
 print(len(x))
+# print(x) # [10, 20, 30, 50]
