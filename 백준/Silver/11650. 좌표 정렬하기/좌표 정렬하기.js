389cec7ac78239a1dd1f1be65471db3dfc3fb880
kanghaeven <122589773+kanghaeven@users.noreply.github.com>
2023-06-15 10:16:40 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 460 ms, Memory: 59232 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -1,13 +1,21 @@
-const [n, ...coords] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-console.log(
-    coords
-    .map((v => ([parseInt(v.split(' ')[0]), parseInt(v.split(' ')[1])])))
-    .sort((a, b) => {
-        if (a[0] !== b[0]) {
-            return a[0] - b[0];
+const n = input.shift();
+
+const num = input.map(ele =>
+    ele.split(' ').map(nums => parseInt(nums)))
+
+let ans = '';
+
+num
+    .sort((prev, cur) => {
+        if (prev[0] !== cur[0]) {
+            return prev[0] - cur[0];
         }
-        return a[1] - b[1];
+        return prev[1] - cur[1];
     })
-    .map(v => String(v[0]) + " " + String(v[1])).join('\n')
-);
+    .forEach(ele => {
+        ans += `${ele[0]} ${ele[1]}\n`
+    })
+
+console.log(ans)
