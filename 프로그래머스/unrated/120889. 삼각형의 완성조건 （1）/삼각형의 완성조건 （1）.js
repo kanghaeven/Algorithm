3b12bab16a48dd20e6a0dde066e003637270b78b
kanghaeven <meerhae@gmail.com>
2023-12-05 12:06:08 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,21 @@
+function solution(sides) {
+    var answer = 0;
+    var maxSide = Math.max(...sides); // 가장 긴 변 찾기
+    var sumOfOtherTwo = 0;
+
+    // 가장 긴 변을 제외한 나머지 두 변의 길이의 합 계산
+    for (var i = 0; i < sides.length; i++) {
+        if (i !== sides.indexOf(maxSide)) {
+            sumOfOtherTwo += sides[i];
+        }
+    }
+
+    // 가장 긴 변의 길이가 나머지 두 변의 길이의 합보다 작으면 삼각형 가능
+    if (maxSide < sumOfOtherTwo) {
+        answer = 1;
+    } else {
+        answer = 2;
+    }
+
+    return answer;
+}
