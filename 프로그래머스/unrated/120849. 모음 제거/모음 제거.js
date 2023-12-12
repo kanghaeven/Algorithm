8826d90ff84744871410ea398c116102fc0dd441
kanghaeven <meerhae@gmail.com>
2023-12-12 15:06:55 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    const aeiou = ['a', 'e', 'i', 'o', 'u'];
-    const arr = my_string.split('');
-    arr.forEach(e => {
-        aeiou.forEach(ee => {
-            if (e === ee ) {
-                my_string = my_string.replace(e, '');
-            }
-        })
-    })
-    return my_string;
+    return my_string.replace(/[aeiou]/g, '');
 }
