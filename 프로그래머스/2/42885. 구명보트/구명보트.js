2397b2a2ed1256d5060b94184911f92dadc9d9db
kanghaeven <meerhae@gmail.com>
2024-01-26 11:11:36 +0900

    [level 2] Title: 구명보트, Time: 34.68 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    var answer = 0;
+    const arr = people.sort((a, b) => b - a);
+    let i = 0;
+    let j = arr.length - 1;
+    
+    while (j >= i) {
+        if (arr[i] + arr[j] <= limit) {
+            i++;
+            j--;
+        } else {
+            i++;
+        }
+        answer++;
+    }
+    return answer;
+}
