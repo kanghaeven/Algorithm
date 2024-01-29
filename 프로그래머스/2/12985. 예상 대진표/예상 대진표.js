396c7c1f17a938137be1e009b1a74ac7843038f2
kanghaeven <meerhae@gmail.com>
2024-01-29 11:05:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n,a,b) {
+    let answer = 0;
+    const arr = [a, b];
+    arr.sort((a, b) => a-b);
+    a = arr[0];
+    b = arr[1];
+    while (a % 2 !== 1 || b % 2 !== 0 || b - a !== 1) {
+        answer++;
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+    };
+    return answer + 1;
+}
