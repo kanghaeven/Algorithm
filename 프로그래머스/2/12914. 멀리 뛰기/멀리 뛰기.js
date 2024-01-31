9a9e5e23561e5a66e4eec87c1aa0ce47b22b0d94
kanghaeven <meerhae@gmail.com>
2024-01-31 11:13:24 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -5,11 +5,10 @@ function solution(n) {
     answer[2] = 2;
     
     for (let i = 3; i <= n; i++) {
-        answer[i] = (answer[i - 1] * 2 - (answer[i - 1] - answer[i - 2])) % 1234567;
-        console.log(i, answer[i - 1] * 2 , Math.floor(i / 2), answer[i]);
+        answer[i] = (answer[i - 1] + answer[i - 2]) % 1234567;
     };
     
-    return answer[n] % 1234567;
+    return answer[n];
 }
 
 // 한번에 1칸 or 2칸
@@ -17,5 +16,3 @@ function solution(n) {
 // if (n === 3) answer = (x-1) * 2 - 1 = 4 - 1 = 3;
 // if (n === 4) answer = (x-1) * 2 - 2 = 6 - 1 = 5;
 // if (n === 5) answer = (x-1) * 2 - 2 = 10 - 2 = 8;
-// if (n === 6) answer = (x-1) * 2 - 3 = 16 - 3 = 13;
-// if (n === 7) answer = (x-1) * 2 - 3 = 26 - 3 = 13;
