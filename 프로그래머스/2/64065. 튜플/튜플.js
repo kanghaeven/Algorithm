9ceee3511399d17814120c930be45bd43522db95
kanghaeven <meerhae@gmail.com>
2024-02-15 11:33:04 +0900

    [level 2] Title: 튜플, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,23 +1,21 @@
-function solution(s) {
-    const tuple = [];
+function solution(s) {    
     const arr = [];
     s.slice(2, s.length-2).split("},{").map(e => {
         arr.push(e);
     });
     arr.sort((a, b) => a.length - b.length);
-    let prev = []
+    
+    const tuple = [];
+    let prev = [];
     arr.forEach((a, aidx) => {
         a.split(",").map(t => {
             if (aidx > 0) {
                 prev = arr[aidx-1].length > 0 ? arr[aidx-1].split(",").map(Number) : [];
-                
-            }
+            };
             const tmp = parseInt(t);
             if (prev.length > 0) {
-                // console.log("여기",tmp, prev.includes(tmp))
                 if (prev.includes(tmp) === false) {
                     tuple.push(tmp);
-                    // console.log("튜플2", prev, tmp, tuple)
                 }
             } else {
                 tuple.push(tmp);
