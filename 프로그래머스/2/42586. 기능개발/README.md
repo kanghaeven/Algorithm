3b8c6332c42999a83e3a49bb4e030dde908a832f
kanghaeven <meerhae@gmail.com>
2024-02-16 12:37:14 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 3.60 ms
+메모리: 33.5 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 12:32:41
+2024년 2월 5일 12:37:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,6 +1,7 @@
 function solution(progresses, speeds) {
-    var answer = [];
+    let answer = [];
     let stack = [];
+    
     progresses.forEach((p, pidx) => {
         for (let i = 0; i < 100; i++) {
             if (p + speeds[pidx]*i >= 100) {
@@ -9,14 +10,12 @@ function solution(progresses, speeds) {
             }
         }
     })
-    console.log(stack)
+
     while (stack.length > 0) {
         for (let q = 0; q < stack.length; q++) {
             let a = 1;
             const tmp = stack.shift();
-            console.log("ddd", tmp, stack)
             for (let j = 0; j <= stack.length; j++) {
-                console.log(j, stack)
                 if (tmp >= stack[j]) {
                     a++;
                 } else {
@@ -24,7 +23,6 @@ function solution(progresses, speeds) {
                     break;
                 }  
             }
-            console.log(stack, tmp, a);
             answer.push(a);
             if (stack.length <= 0) {
                 break;
@@ -34,9 +32,3 @@ function solution(progresses, speeds) {
  
     return answer;
 }
-
-// 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고,
-// 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됨
-
-// 각 배포마다 몇 개의 기능이 배포되는지 return
-// 배포는 하루에 한번 가능
