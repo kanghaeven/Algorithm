9a5d9130df7603186a7540cf975021e8ac2ea158
kanghaeven <meerhae@gmail.com>
2024-02-17 12:16:31 +0900

    [level 2] Title: 프로세스, Time: 4.71 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,17 @@
+function solution(priorities, location) {
+    let answer = 0;
+    const queue = priorities.map((priority, index) => ({ index, priority }));
+    console.log(queue)
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+        if (queue.some(item => item.priority > current.priority)) {
+            queue.push(current);
+        } else {
+            answer++;
+            if (current.index === location) {
+                return answer;
+            }
+        }
+    }
+}
