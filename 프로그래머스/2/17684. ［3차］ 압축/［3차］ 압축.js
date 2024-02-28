fa24352f722d9ee60d4f3f6a4eac20b5b8769763
kanghaeven <meerhae@gmail.com>
2024-02-28 17:55:38 +0900

    [level 2] Title: [3차] 압축, Time: 0.97 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,19 +1,20 @@
 function solution(msg) {
     let answer = [];
-    let array = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 65)); // 'A'(65)부터 'Z'(90)까지 초기화
-   let stack = []
-const result = []
-for(let i=0; i<msg.length;i++) {
-stack.push(msg[i])
-if(array.includes(stack.join(''))) continue;
-    array.push(stack.join(''));
-    stack.pop();
+    let array = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 65));
+    let stack = [];
+    const result = [];
+    for (let i = 0; i < msg.length; i++) {
+        stack.push(msg[i]);
+        if (array.includes(stack.join(''))) continue;
+        array.push(stack.join(''));
+        stack.pop();
+        const index = array.indexOf(stack.join('')) + 1;
+        result.push(index);
+        stack = [];
+        i--;
+    }
     const index = array.indexOf(stack.join('')) + 1;
     result.push(index);
-    stack = [];
-    i--;
-}
-    const index = array.indexOf(stack.join(''))+ 1;
-    result.push(index);
-return result
+    
+    return result;
 }
