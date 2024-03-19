19a7cc62fe286c948b9cb72d2d12d56455d03303
kanghaeven <meerhae@gmail.com>
2024-03-19 18:05:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,20 +1,8 @@
 function solution(num) {
-    var answer = 0;
-    let n = num
-    while (n > 0) {
-        if (n === 1) break;
-        if (n % 2 === 0) {
-            n = n / 2;
-        } else {
-            n = n * 3 + 1;
-        }
+    let answer = 0;
+    while (num !== 1 && answer !== 500) {
+        num % 2 == 0 ? num = num / 2 : num = num * 3 + 1;
         answer++;
-        if (n === 1) break;
-        if (answer === 500) {
-            answer = -1;
-            break;
-        }
-    }
-    
-    return answer;
+  }
+    return num == 1 ? answer : -1;
 }
