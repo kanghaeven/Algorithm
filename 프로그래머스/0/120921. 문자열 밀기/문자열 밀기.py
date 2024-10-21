02c7376885281fa8c5fd0af5cee03b7d88a603d2
kanghaeven <meerhae@gmail.com>
2024-10-21 21:34:08 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,9 @@
+def solution(A, B):
+    if A == B: 
+        return 0
+    for i in range(1, len(A)):
+        A = A[-1] + A[0:-1]
+        print(i, A)
+        if A == B:
+            return i
+    return -1
