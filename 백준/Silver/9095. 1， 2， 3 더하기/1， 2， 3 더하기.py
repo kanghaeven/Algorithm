a1df101b38869e6e67e1c175d3fb5b9ff772a0f5
kanghaeven <meerhae@gmail.com>
2025-02-26 13:29:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,21 +1,20 @@
-T = int(input())
+# 1 1
+# 2 2
+# 3 4
+# 4 7 = 1 + 2 + 4
+# 5 13 = 2 + 4 + 7
+# 6 24 = 4 + 7 + 13
+# 7 44 = 7 + 13 + 24
 
-memo = [0] * 11
+t = int(input())
+dp = [0 for _ in range(12)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-def plus(x):
-    if x == 1:
-        return 1
-    if x == 2:
-        return 2
-    if x == 3:
-        return 4
-    
-    if memo[x] != 0:
-        return memo[x]
-    
-    memo[x] = plus(x-1) + plus(x-2) + plus(x-3)
-    return memo[x]
-
-for i in range(T):
-    N = int(input())
-    print(plus(N))
+for _ in range(t):
+    n = int(input())
+    if dp[n] == 0:
+        for i in range(4, n + 1):
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+    print(dp[n])
